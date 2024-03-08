557776713fb0c1427c953eaf5af229e59b9c4051
kaikaci12 <somewonesomewone@gmail.com>
2024-03-08 15:54:58 +0400

    set up a changeOwed function that returns leftover and if the user input is invalid it asks user again to make cash

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const cashOwed = parseInt(prompt("Make Cash in Cents: "));
+function changeOwed(cash) {
+  while (isNaN(cash) || cash < 1 || cash > 100) {
+    cash = parseInt(prompt("Make Cash in Cents: "));
+  }
+}
+
+// console.log(` The smallest amount of coins I can return is: ${minCoins()}`)
