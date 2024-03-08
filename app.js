58d0ebd2528a3e58cbdf3531001470db97a0a467
kaikaci12 <somewonesomewone@gmail.com>
2024-03-08 15:56:33 +0400

    create variable change that calculates the change from 1$ and finally return the value

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function changeOwed(cash) {
   while (isNaN(cash) || cash < 1 || cash > 100) {
     cash = parseInt(prompt("Make Cash in Cents: "));
   }
+  const change = 100 - cash;
+  return change;
 }
-
 // console.log(` The smallest amount of coins I can return is: ${minCoins()}`)
